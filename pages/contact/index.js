4ab123a19c9092c76979cd7f6e195e1f19633258
jakir442 <145217412+jakir442@users.noreply.github.com>
2023-12-02 11:54:53 +0700

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -17,9 +17,9 @@ const Contact = () => {
         {/* text & form */}
         <div className="flex flex-col w-full max-w-[700px] ">
           {/* text */}
-          <motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden" className="h2 text-center mb-12">
-            let's <span className="text-accent">connect.</span>
-          </motion.h2>
+          // <motion.h2 variants={fadeIn('up', 0.2)} initial="hidden" animate="show" exit="hidden" className="h2 text-center mb-12">
+          //   let's <span className="text-accent"></span>
+          // </motion.h2>
           {/* form */}
           <motion.form variants={fadeIn('up', 0.4)} initial="hidden" animate="show" exit="hidden" className="flex-1 flex flex-col gap-6 w-full mx-auto">
             <div className="flex gap-x-6 w-full">
